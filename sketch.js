459d775d71a646a7a95928c006d405408a6e982b
Varshitha1230 <85627031+Varshitha1230@users.noreply.github.com>
2021-06-27 21:44:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+var garden,rabbit;
+var gardenImg,rabbitImg;
+
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  
+}
+
+
+function setup(){
+  
+  createCanvas(400,400);
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+
+//creating boy running
+rabbit = createSprite(160,340,20,20);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+function draw() {
+  background(0);
+  
+  // boy moving on Xaxis with mouse'
+  rabbit.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+  
+   drawSprites();
+  
+}
+
+   
